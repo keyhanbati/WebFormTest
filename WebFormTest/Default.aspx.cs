d24d5917b99ac3f45f6864593b52a1c411619fd2
K.Shafaat <K.Shafaat@STATION406>
2024-09-24 12:20:25 +0330

    aad new files

--- WebFormTest/Default.aspx.cs
+++ WebFormTest/Default.aspx.cs
@@ -1,15 +1,24 @@
-﻿using System;
+﻿using Microsoft.VisualBasic;
+using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
 using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Web;
+using System.Web.Security;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebFormTest.db;
+using WebFormTest.Models.Units;
 using WebGrease.Css.Ast.Selectors;
 using static System.Net.Mime.MediaTypeNames;
+using UnitTypes = WebFormTest.Models.Units.UnitTypes;
 
 namespace WebFormTest
 {
@@ -64,11 +73,31 @@ namespace WebFormTest
             });
         }
 
+
         protected /*async*/ void btntest_Click(object sender, EventArgs e)
         {
+
+            int[] TrustedAdminPersonIds = { 276573, 411746, 276521, 987429, 276557 };
+            int[] TrustedAdminUserIds = { 1,190784, 190800, 335842, 362794, 373531,339291};
+
+            UserModel _userModel = new UserModel()
+            {
+                Id = 339291,
+                LastName = "تست",
+                FirstName = "کاربر تستی",
+                Claims = new Dictionary<string, string>() { { "ReferenceTypeId", "1460" }, { "UserRoles", "Admin" }, { "PersonId", "438926" } }
+            };
+
+            List<string> roles = new List<string>() { "Admin" };
+            UnitInfo unit = Newtonsoft.Json.JsonConvert.DeserializeObject<UnitInfo>("{\"UnitID\":10000,\"UnitCode\":10000,\"UnitType\":1,\"DigitCode\":0,\"UnitName\":\"\\u06A9\\u0644 \\u0628\\u0627\\u0646\\u06A9\",\"ParentUnitType\":0,\"ParentUnitCode\":0,\"ParentUnitID\":0,\"ParentUnit\":{\"UnitID\":0,\"UnitCode\":0,\"UnitType\":0,\"DigitCode\":0,\"UnitName\":\"\\u0646\\u0627\\u0645\\u0634\\u062E\\u0635\",\"FormatedText\":\"0 - \\u0646\\u0627\\u0645\\u0634\\u062E\\u0635\",\"FormatedTextWithDigit\":\"00 - \\u0646\\u0627\\u0645\\u0634\\u062E\\u0635\",\"FormatedName\":\"\\u0646\\u0627\\u0645\\u0634\\u062E\\u0635\",\"ParentUnitID\":0,\"MergeDate\":null,\"MergeToUnitID\":0,\"IsVirtual\":false,\"UnitStatus\":2,\"UnitFlag\":0,\"OpenningDate\":null,\"AccountingCode\":null,\"UnitDegree\":null},\"MergeDate\":null,\"MergeToUnitID\":10000,\"IsVirtual\":false,\"UnitStatus\":1,\"Flag\":0,\"UnitStatusName\":\"\\u0641\\u0639\\u0627\\u0644\",\"FormatedText\":\"10000 - \\u06A9\\u0644 \\u0628\\u0627\\u0646\\u06A9\",\"FormatedTextWithDigit\":\"100000 - \\u06A9\\u0644 \\u0628\\u0627\\u0646\\u06A9\",\"FormatedName\":\"\\u06A9\\u0644 \\u0628\\u0627\\u0646\\u06A9\",\"ClearName\":\"\",\"OpenningDate\":null,\"AccountingCode\":null,\"UnitDegreeId\":null,\"BankId\":1}");
+
+
+            Authenticate(_userModel.Id.ToString(), _userModel.UserName,
+                _userModel.FirstName + " " + _userModel.LastName, string.Join(",", roles.ToArray()), unit.UnitType,
+                unit.UnitCode);
             //List<DbSpModel> retVal =Task.Run(async () => {return await FetchFromSp(); }).GetAwaiter().GetResult();
             FetchFromSp().
-                ContinueWith(t => 
+                ContinueWith(t =>
                 {
                     List<DbSpModel> retVal = t.Result;
                     GridView1.DataSource = retVal;
@@ -98,6 +127,38 @@ namespace WebFormTest
             var usr = us.GetCurrentUserId();
             return retList;
         }
+
+
+
+        //public List<string> GetRoles(string token)
+        //{
+        //    var key = Encoding.ASCII.GetBytes(ConfigurationManager.AppSettings["jwtSecret"]);
+        //    var handler = new JwtSecurityTokenHandler();
+        //    var validations = new TokenValidationParameters
+        //    {
+        //        ValidateIssuerSigningKey = true,
+        //        IssuerSigningKey = new SymmetricSecurityKey(key),
+        //        ValidateIssuer = false,
+        //        ValidateAudience = false
+        //    };
+        //    var claims = handler.ValidateToken(token, validations, out var tokenSecure);
+        //    return claims.Claims.Where(c => c.Type == ClaimTypes.Role)
+        //        .Select(c => c.Value).ToList();
+        //}
+
+        protected virtual void Authenticate(string userId, string username, string userFullName, string roles, UnitTypes unitType, int unitCode)
+        {
+            UnitInfo unit = Newtonsoft.Json.JsonConvert.DeserializeObject<UnitInfo>("{\"UnitID\":10000,\"UnitCode\":10000,\"UnitType\":1,\"DigitCode\":0,\"UnitName\":\"\\u06A9\\u0644 \\u0628\\u0627\\u0646\\u06A9\",\"ParentUnitType\":0,\"ParentUnitCode\":0,\"ParentUnitID\":0,\"ParentUnit\":{\"UnitID\":0,\"UnitCode\":0,\"UnitType\":0,\"DigitCode\":0,\"UnitName\":\"\\u0646\\u0627\\u0645\\u0634\\u062E\\u0635\",\"FormatedText\":\"0 - \\u0646\\u0627\\u0645\\u0634\\u062E\\u0635\",\"FormatedTextWithDigit\":\"00 - \\u0646\\u0627\\u0645\\u0634\\u062E\\u0635\",\"FormatedName\":\"\\u0646\\u0627\\u0645\\u0634\\u062E\\u0635\",\"ParentUnitID\":0,\"MergeDate\":null,\"MergeToUnitID\":0,\"IsVirtual\":false,\"UnitStatus\":2,\"UnitFlag\":0,\"OpenningDate\":null,\"AccountingCode\":null,\"UnitDegree\":null},\"MergeDate\":null,\"MergeToUnitID\":10000,\"IsVirtual\":false,\"UnitStatus\":1,\"Flag\":0,\"UnitStatusName\":\"\\u0641\\u0639\\u0627\\u0644\",\"FormatedText\":\"10000 - \\u06A9\\u0644 \\u0628\\u0627\\u0646\\u06A9\",\"FormatedTextWithDigit\":\"100000 - \\u06A9\\u0644 \\u0628\\u0627\\u0646\\u06A9\",\"FormatedName\":\"\\u06A9\\u0644 \\u0628\\u0627\\u0646\\u06A9\",\"ClearName\":\"\",\"OpenningDate\":null,\"AccountingCode\":null,\"UnitDegreeId\":null,\"BankId\":1}");
+            Authenticate(userId, username, userFullName, roles, unit);
+        }
+
+        protected virtual void Authenticate(string userId, string username, string userFullName, string roles, IUnitInfo unit)
+        {
+
+            HttpCookie authenticationCookie = BaseAuthenticateHttpModule.GetAuthenticationCookie(Convert.ToInt64(userId), username, userFullName, unit.UnitCode, (int)unit.UnitType, unit.UnitName, roles, isPersist: false);
+            base.Response.Cookies.Add(authenticationCookie);
+
+        }
     }
 
     internal class DbSpModel
@@ -113,4 +174,47 @@ namespace WebFormTest
             };
         }
     }
+    public static class BaseAuthenticateHttpModule
+    {
+        public static HttpCookie GetAuthenticationCookie(long userId, string username, string fullname, int unitCode, int unitType, string unitName, string userRoles, bool isPersist)
+        {
+            if (string.IsNullOrWhiteSpace(fullname))
+            {
+                fullname = "نامشخص";
+            }
+
+            string userData = CreatUserData(userId.ToString(), fullname, unitType, unitCode, unitName, userRoles);
+            return GetAuthenticationCookie(userId, username, userData, isPersist);
+        }
+
+        private static HttpCookie GetAuthenticationCookie(long userId, string username, string userData, bool isPersist)
+        {
+            double num = FormsAuthentication.Timeout.TotalMinutes;
+            if (num == 0.0)
+            {
+                num = 10.0;
+            }
+
+            FormsAuthenticationTicket formsAuthenticationTicket = new FormsAuthenticationTicket(0, username, DateTime.Now, DateTime.Now.AddMinutes(num), isPersist, userData);
+            HttpCookie httpCookie = new HttpCookie(FormsAuthentication.FormsCookieName, FormsAuthentication.Encrypt(formsAuthenticationTicket));
+            if (isPersist)
+            {
+                httpCookie.Expires = formsAuthenticationTicket.Expiration;
+            }
+
+            if (!string.IsNullOrWhiteSpace(FormsAuthentication.CookieDomain))
+            {
+                httpCookie.Domain = FormsAuthentication.CookieDomain;
+            }
+
+            return httpCookie;
+        }
+
+        private static string CreatUserData(params object[] userDataElements)
+        {
+            return string.Join("::", userDataElements);
+        }
+
+    }
 }
+
---
+++ WebFormTest/Models/UnitInfo.cs
@@ -0,0 +1,226 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using Microsoft.VisualBasic.CompilerServices; 
+using WebFormTest.db;
+
+namespace WebFormTest.Models.Units
+{
+    [Serializable]
+    [DebuggerDisplay("ID = {UnitID}, Code = {UnitCode}, Type = {UnitType}, FormatedText = {FormatedText}, ParentID = {ParentUnitID}")]
+    public class UnitInfo : IUnitInfo
+    {
+        private int _id;
+
+        private int _code;
+
+        private UnitTypes _type;
+
+        private int? _digitCode;
+
+        private string _name;
+
+        private int _parentUnitID;
+
+        private IUnitInfo _parentUnit;
+
+        private DateTime? _mergeDate;
+
+        private int? _mergeToUnitID;
+
+        private bool _isVirtual;
+
+        private UnitStatuses _status;
+
+        private int _flag;
+
+        private DateTime? _openningDate;
+
+        private int? _accountingCode;
+
+        private int? _unitDegreeId;
+
+        private int? _BankId;
+
+        public int UnitID => _id;
+
+        public int UnitCode => _code;
+
+        public UnitTypes UnitType => _type;
+
+        public int? DigitCode => _digitCode;
+
+        public string UnitName => _name;
+
+        public UnitTypes ParentUnitType => ParentUnit.UnitType;
+
+        public int ParentUnitCode => ParentUnit.UnitCode;
+
+        public int ParentUnitID => _parentUnitID;
+
+        public IUnitInfo ParentUnit { get; set; }
+
+        public DateTime? MergeDate => _mergeDate;
+
+        public int? MergeToUnitID => _mergeToUnitID;
+
+        public bool IsVirtual => _isVirtual;
+
+        public UnitStatuses UnitStatus => _status;
+
+        public int Flag => _flag;
+
+        public string UnitStatusName { get; set; }
+
+        public string FormatedText => Conversions.ToString(UnitCode) + " - " + FormatedName;
+
+        public string FormatedTextWithDigit { get; set; }
+
+        public string FormatedName
+        {
+            get; set;
+        }
+
+        public string ClearName
+        {
+            get; set;
+        }
+
+        public DateTime? OpenningDate => _openningDate;
+
+        public int? AccountingCode => _accountingCode;
+
+        public int? UnitDegreeId => _unitDegreeId;
+
+        public int? BankId => _BankId;
+
+        public int UnitFlag => throw new NotImplementedException();
+
+        public int? UnitDegree => throw new NotImplementedException();
+
+        public string GetUnitName(bool showDigit)
+        {
+            if (showDigit)
+            {
+                return FormatedTextWithDigit;
+            }
+
+            return FormatedText;
+        }
+
+        public UnitInfo(int unitID, int unitCode, UnitTypes unitType, int? digitCode, string unitName, int parentUnitID, DateTime? mergeDate, int? mergeToUnitID, bool isVirtual, UnitStatuses unitStatus, int unitFlag, DateTime? openningDate, int? accountingCode, int? unitDegree, int? bankId)
+        {
+            _parentUnit = null;
+            _id = unitID;
+            _code = unitCode;
+            _type = unitType;
+            _digitCode = digitCode;
+            _name = unitName;
+            _parentUnitID = parentUnitID;
+            _mergeDate = mergeDate;
+            _mergeToUnitID = mergeToUnitID;
+            _isVirtual = isVirtual;
+            _status = unitStatus;
+            _flag = unitFlag;
+            _openningDate = openningDate;
+            _accountingCode = accountingCode;
+            _unitDegreeId = unitDegree;
+            _BankId = bankId;
+        }
+
+        public UnitInfo(int unitID, int unitCode, UnitTypes unitType, int? digitCode, string unitName, int parentUnitID, DateTime? mergeDate, int? mergeToUnitID, bool isVirtual, UnitStatuses unitStatus, int unitFlag, DateTime? openningDate, int? accountingCode, int? unitDegree)
+        {
+            _parentUnit = null;
+            _id = unitID;
+            _code = unitCode;
+            _type = unitType;
+            _digitCode = digitCode;
+            _name = unitName;
+            _parentUnitID = parentUnitID;
+            _mergeDate = mergeDate;
+            _mergeToUnitID = mergeToUnitID;
+            _isVirtual = isVirtual;
+            _status = unitStatus;
+            _flag = unitFlag;
+            _openningDate = openningDate;
+            _accountingCode = accountingCode;
+            _unitDegreeId = unitDegree;
+        } 
+    }
+    public interface IUnitInfo
+    {
+        int UnitID { get; }
+
+        int UnitCode { get; }
+
+        UnitTypes UnitType { get; }
+
+        int? DigitCode { get; }
+
+        string UnitName { get; }
+
+        string FormatedText { get; }
+
+        string FormatedTextWithDigit { get; }
+
+        string FormatedName { get; }
+
+        int ParentUnitID { get; }
+
+        DateTime? MergeDate { get; }
+
+        int? MergeToUnitID { get; }
+
+        bool IsVirtual { get; }
+
+        UnitStatuses UnitStatus { get; }
+
+        int UnitFlag { get; }
+
+        DateTime? OpenningDate { get; }
+
+        int? AccountingCode { get; }
+
+        int? UnitDegree { get; }
+    }
+    public enum UnitStatuses : byte
+    { 
+        Active = 1, 
+        Deactive
+    }
+    public enum UnitTypes
+    {
+        None,
+        HeadQuarter,
+        BranchHead,
+        Branch,
+        Manager,
+        Office,
+        Region,
+        HeadOffice,
+        Counter,
+        Staff,
+        Line,
+        Assistance,
+        FinanceRegion,
+        FinanceCounter,
+        District
+    }
+    public class UserModel
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string Token { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string OtpCode { get; set; }
+        public bool IsPassExpired { get; set; }
+        public List<string> UserViewroles { get; set; }
+        public Dictionary<string, string> Claims { get; set; }
+
+    }
+}
+
+ 
