f8212c3182f6be2ce201ca8a72f5ca0e1b2acc27
K.Shafaat <K.Shafaat@STATION406>
2024-09-24 15:20:04 +0330

    bb

--- WebFormTest/Models/UnitInfo.cs
+++ WebFormTest/Models/UnitInfo.cs
@@ -58,7 +58,150 @@ namespace WebFormTest.Models.Units
 
         public int ParentUnitID => _parentUnitID;
 
-        public IUnitInfo ParentUnit { get; set; }
+        public UnitInfo ParentUnit { get; set; }
+
+        public DateTime? MergeDate => _mergeDate;
+
+        public int? MergeToUnitID => _mergeToUnitID;
+
+        public bool IsVirtual => _isVirtual;
+
+        public UnitStatuses UnitStatus => _status;
+
+        public int Flag => _flag;
+
+        public string UnitStatusName { get; set; }
+
+        public string FormatedText => Conversions.ToString(UnitCode) + " - " + FormatedName;
+
+        public string FormatedTextWithDigit { get; set; }
+
+        public string FormatedName
+        {
+            get; set;
+        }
+
+        public string ClearName
+        {
+            get; set;
+        }
+
+        public DateTime? OpenningDate => _openningDate;
+
+        public int? AccountingCode => _accountingCode;
+
+        public int? UnitDegreeId => _unitDegreeId;
+
+        public int? BankId => _BankId;
+
+        public int UnitFlag => throw new NotImplementedException();
+
+        public int? UnitDegree => throw new NotImplementedException();
+
+        public string GetUnitName(bool showDigit)
+        {
+            if (showDigit)
+            {
+                return FormatedTextWithDigit;
+            }
+
+            return FormatedText;
+        }
+        public UnitInfo()
+        {
+        }
+
+        public UnitInfo(int unitID, int unitCode, UnitTypes unitType, int? digitCode, string unitName, int parentUnitID, DateTime? mergeDate, int? mergeToUnitID, bool isVirtual, UnitStatuses unitStatus, int unitFlag, DateTime? openningDate, int? accountingCode, int? unitDegree, int? bankId)
+        {
+            _parentUnit = null;
+            _id = unitID;
+            _code = unitCode;
+            _type = unitType;
+            _digitCode = digitCode;
+            _name = unitName;
+            _parentUnitID = parentUnitID;
+            _mergeDate = mergeDate;
+            _mergeToUnitID = mergeToUnitID;
+            _isVirtual = isVirtual;
+            _status = unitStatus;
+            _flag = unitFlag;
+            _openningDate = openningDate;
+            _accountingCode = accountingCode;
+            _unitDegreeId = unitDegree;
+            _BankId = bankId;
+        }
+
+        public UnitInfo(int unitID, int unitCode, UnitTypes unitType, int? digitCode, string unitName, int parentUnitID, DateTime? mergeDate, int? mergeToUnitID, bool isVirtual, UnitStatuses unitStatus, int unitFlag, DateTime? openningDate, int? accountingCode, int? unitDegree)
+        {
+            _parentUnit = null;
+            _id = unitID;
+            _code = unitCode;
+            _type = unitType;
+            _digitCode = digitCode;
+            _name = unitName;
+            _parentUnitID = parentUnitID;
+            _mergeDate = mergeDate;
+            _mergeToUnitID = mergeToUnitID;
+            _isVirtual = isVirtual;
+            _status = unitStatus;
+            _flag = unitFlag;
+            _openningDate = openningDate;
+            _accountingCode = accountingCode;
+            _unitDegreeId = unitDegree;
+        }
+
+    } 
+    public class UnitInfo2 
+    {
+        private int _id;
+
+        private int _code;
+
+        private UnitTypes _type;
+
+        private int? _digitCode;
+
+        private string _name;
+
+        private int _parentUnitID;
+
+        private IUnitInfo _parentUnit;
+
+        private DateTime? _mergeDate;
+
+        private int? _mergeToUnitID;
+
+        private bool _isVirtual;
+
+        private UnitStatuses _status;
+
+        private int _flag;
+
+        private DateTime? _openningDate;
+
+        private int? _accountingCode;
+
+        private int? _unitDegreeId;
+
+        private int? _BankId;
+
+        public int UnitID => _id;
+
+        public int UnitCode => _code;
+
+        public UnitTypes UnitType => _type;
+
+        public int? DigitCode => _digitCode;
+
+        public string UnitName => _name;
+
+        public UnitTypes ParentUnitType => ParentUnit.UnitType;
+
+        public int ParentUnitCode => ParentUnit.UnitCode;
+
+        public int ParentUnitID => _parentUnitID;
+
+        public UnitInfo2 ParentUnit { get; set; }
 
         public DateTime? MergeDate => _mergeDate;
 
@@ -108,43 +251,8 @@ namespace WebFormTest.Models.Units
             return FormatedText;
         }
 
-        public UnitInfo(int unitID, int unitCode, UnitTypes unitType, int? digitCode, string unitName, int parentUnitID, DateTime? mergeDate, int? mergeToUnitID, bool isVirtual, UnitStatuses unitStatus, int unitFlag, DateTime? openningDate, int? accountingCode, int? unitDegree, int? bankId)
-        {
-            _parentUnit = null;
-            _id = unitID;
-            _code = unitCode;
-            _type = unitType;
-            _digitCode = digitCode;
-            _name = unitName;
-            _parentUnitID = parentUnitID;
-            _mergeDate = mergeDate;
-            _mergeToUnitID = mergeToUnitID;
-            _isVirtual = isVirtual;
-            _status = unitStatus;
-            _flag = unitFlag;
-            _openningDate = openningDate;
-            _accountingCode = accountingCode;
-            _unitDegreeId = unitDegree;
-            _BankId = bankId;
-        }
-
-        public UnitInfo(int unitID, int unitCode, UnitTypes unitType, int? digitCode, string unitName, int parentUnitID, DateTime? mergeDate, int? mergeToUnitID, bool isVirtual, UnitStatuses unitStatus, int unitFlag, DateTime? openningDate, int? accountingCode, int? unitDegree)
-        {
-            _parentUnit = null;
-            _id = unitID;
-            _code = unitCode;
-            _type = unitType;
-            _digitCode = digitCode;
-            _name = unitName;
-            _parentUnitID = parentUnitID;
-            _mergeDate = mergeDate;
-            _mergeToUnitID = mergeToUnitID;
-            _isVirtual = isVirtual;
-            _status = unitStatus;
-            _flag = unitFlag;
-            _openningDate = openningDate;
-            _accountingCode = accountingCode;
-            _unitDegreeId = unitDegree;
+        public UnitInfo2()
+        { 
         } 
     }
     public interface IUnitInfo
